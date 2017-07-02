5a392cb7faf14684cdabba9d239f93f2b3db6833
Sahit Chintalapudi <chsahit@gmail.com>
2017-07-02 17:35:34 -0400

    skeleton of macaroni controls

--- macaroni/motorControl/motorControl2017.ino
+++ macaroni/motorControl/motorControl2017.ino
@@ -1,6 +1,25 @@
 #include <LiquidCrystal.h>
 #include <Servo.h>
 
+void motor(int val);
+void steer(int val);
+void update();
+int limitDesiredSpeed(int desiredSpeed);
+int limitDesiredHeading(int desiredHeading);
+void updateHeading();
+void updateSpeed();
+
+static int currentSpeed = 0;
+static int desiredSpeed=0;
+static int currentHeading = 0;
+static int desiredHeading = 0;
+
+//control limits
+static const int maxSpeed = 30; // maximum velocity 
+static const int minSpeed = -15;
+static const int minSteer = -25;
+static const int maxSteer = 25;
+
 void setup()
 
 }
@@ -10,4 +29,32 @@ void loop()
 
 }
 
+void update()
+{
+    if(getMessage()) {
+        desiredSpeed = limitDesiredSpeed(desiredSpeed);
+        desiredHeading = limitDesiredHeading(desiredHeading); 
+    }
+    updateHeading();
+    updateSpeed();
+}
 
+void limitDesiredSpeed(int desiredSpeed) {
+    return min(maxSpeed, max(requestedSpeed, minSpeed));
+}
+
+void limitDesiredHeading(int desiredHeading) {
+    return min(maxHeading, max(requestedHeading, minHeading));    
+}
+
+void updateHeading()
+{
+    
+}
+/*
+    - encoder input -> PID -> writing to motor
+    - encoder input -> back to Joule
+    - read the mux state -> tell PID what do
+    - read the E-Stop state -> tell PID what do
+
+*/
