1756029dea484a86766c6da4482a314f2799b633
Evan Bretl <evanbretl4@gmail.com>
2017-09-17 20:51:54 -0400

    var SPEED was an integer

--- carwars/firmware/bigoli_test/bigoli_test.ino
+++ carwars/firmware/bigoli_test/bigoli_test.ino
@@ -1,8 +1,14 @@
+/*
+const int leftDir = 10;
+const int leftSpeed = 9;
+const int rightDir = 7;
+const int rightSpeed = 6;
+*/
 
-const int pin_esc_speed = 1;
-const int pin_esc_direction = 2;
+const int pin_esc_speed = 9;
+const int pin_esc_direction = 10;
 
-int SPEED = 0;
+float SPEED = 0;
 
 bool getMessage() {
     bool gotMessage = false;
@@ -17,30 +23,36 @@ bool getMessage() {
 
 void drive() {
     int direction;
+    float pwm = SPEED;
     if(SPEED >= 0) {
         direction = HIGH;
+        pwm = 1.0 - pwm;
     } else {
         direction = LOW;
-        SPEED = -SPEED;
+        pwm *= -1;
     }
 
-    float pwm = SPEED * 0.1;
-
-    //note: circuitry flips the signal
-    int analogInt = (int) (255 * (1.0 - pwm));
+    int analogInt = (int) (255 * pwm);
 
     digitalWrite(pin_esc_direction, direction);
     analogWrite(pin_esc_speed, analogInt);
 }
 
 void setup() {
+    Serial.begin(9600);
     pinMode(pin_esc_direction, OUTPUT);
     pinMode(pin_esc_speed, OUTPUT);
+    pinMode(13, OUTPUT);
+    digitalWrite(13, LOW);
     drive();
 }
 
 void loop() {
     if(getMessage()) {
         drive();
+        digitalWrite(13, HIGH);
+    } else {
+        digitalWrite(13, LOW);
     }
+    delay(50);
 }
