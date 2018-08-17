84eb0335af42087ba4adaa4b7fb36a845e6ef595
Evan Bretl <evanbretl4@gmail.com>
2018-08-16 21:02:34 -0400

    ethernet "basic functions" drive programs

--- carwars/mbed/usb/drive-motor-forward-backward-cycle.cpp
+++ carwars/mbed/usb/drive-motor-forward-backward-cycle.cpp
@@ -17,22 +17,22 @@ int main()
     {
         serial.printf("a = low, b modulated\r\n");
         driverPinA.write(0.0f);
-        for(float pwm = 0; pwm < 1; pwm += 0.01) {
+        for(float pwm = 0; pwm < 0.3; pwm += 0.01) {
             driverPinB.write(pwm);
             wait_ms(20);
         }
-        for(float pwm = 1; pwm > 0; pwm -= 0.01) {
+        for(float pwm = 0.3; pwm > 0; pwm -= 0.01) {
             driverPinB.write(pwm);
             wait_ms(20);
         }
 
         serial.printf("a modulated, b = low\r\n");
         driverPinB.write(0.0f);
-        for(float pwm = 0; pwm < 1; pwm += 0.01) {
+        for(float pwm = 0; pwm < 0.3; pwm += 0.01) {
             driverPinA.write(pwm);
             wait_ms(20);
         }
-        for(float pwm = 1; pwm > 0; pwm -= 0.01) {
+        for(float pwm = 0.3; pwm > 0; pwm -= 0.01) {
             driverPinA.write(pwm);
             wait_ms(20);
         }
