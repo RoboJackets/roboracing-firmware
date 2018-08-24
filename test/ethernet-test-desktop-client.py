63424231999c3506925d07d6af70bd6c6e80dbe7
jspall0327 <jspall98@aol.com>
2018-08-23 21:25:21 -0400

    Ethernet test script for sending different commands

--- test/ethernet-test-desktop-client.py
+++ test/ethernet-test-desktop-client.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import socket
 import signal
 import sys
+import threading
 
 # Evan's laptop works with: IP whatever, netmask 255.255.255.0, gateway 0.0.0.0
 
@@ -14,15 +15,26 @@ signal.signal(signal.SIGINT, signal_handler)
 
 ECHO_SERVER_ADDRESS = "192.168.2.2"
 ECHO_SERVER_PORT = 7
-message = b'Hello, world'
 
 print("setting up socket...")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print("connecting socket...")
 s.connect((ECHO_SERVER_ADDRESS, ECHO_SERVER_PORT))
+message = "0.0 0.0 0.0 0.0"
 
-print('Sending', str(message))
-s.sendall(message)
-data = s.recv(1024)
-s.close()
-print('Received', str(data))
+def send_commands_loop():
+    global message
+    prev_data = None
+    while (True):
+        # print('Sending', str(message))
+        s.sendall(message)
+        data = str(s.recv(1024))
+        if data != prev_data:
+            print('Received', data)
+        prev_data = data
+    s.close()
+
+threading.Thread(target=send_commands_loop).start()
+
+while (True):
+    message = raw_input()
