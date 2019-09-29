92d9f3a06330b6328ec60f8f87ae4476a0c180b8
Skyhawkson <32376505+Skyhawkson@users.noreply.github.com>
2019-09-29 18:07:01 -0400

    Update stepper_motor.ino
    Updated Stepper Sketch

--- new_cari/stepper_motor/stepper_motor.ino
+++ new_cari/stepper_motor/stepper_motor.ino
@@ -1,19 +1,36 @@
+
+// Notes:
+// Need to connect VDC GND and dir- to common GND
+// Need to have feedback connected to function
+// 15.3 (gear ratio) * 1600 = pulses per revolution
+
 byte dirPin = 3;
 byte pulsePin = 6;
 byte interruptPin = 2;
 
 void setup() {
+  Serial.begin(115200);
   pinMode(dirPin, OUTPUT);
   pinMode(pulsePin, OUTPUT);
   pinMode(interruptPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(interruptPin), interrupt, FALLING);
-  digitalWrite(dirPin, HIGH);
+  digitalWrite(dirPin, LOW); // HIGH: clockwise, LOW: counter clockwise
   delayMicroseconds(30);
+   for(int i=0; i<24480; i++)
+  {
+    delay(.01);
+    Serial.print("tick");
+    digitalWrite(pulsePin, HIGH);
+    delayMicroseconds(20);
+    digitalWrite(pulsePin, LOW);
+    delayMicroseconds(20);
+  }
 }
  
 void loop() {
-  delay(10);
+
 }
+
 
 void interrupt(){
   digitalWrite(pulsePin, HIGH);
