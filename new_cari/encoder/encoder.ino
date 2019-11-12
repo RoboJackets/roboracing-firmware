6837805b024cd41a5aabeba0d51b82c347f5e471
Kyle C <kcarter47@gatech.edu>
2019-11-11 19:42:31 -0500

    Update encoder.ino

--- new_cari/encoder/encoder.ino
+++ new_cari/encoder/encoder.ino
@@ -1,6 +1,62 @@
 // Code found on the CUI website:
 // https://www.cuidevices.com/product/resource/sample-code/amt22
 
+/*
+ * AMT22_SPI_Sample_Code.ino
+ * Company: CUI Inc.
+ * Author: Jason Kelly
+ * Version: 2.0.1.0
+ * Date: August 20, 2019
+ * 
+ * This sample code can be used with the Arduino Uno to control the AMT22 encoder.
+ * It uses SPI to control the encoder and the the Arduino UART to report back to the PC
+ * via the Arduino Serial Monitor.
+ * For more information or assistance contact CUI Inc for support.
+ * 
+ * After uploading code to Arduino Uno open the open the Serial Monitor under the Tools 
+ * menu and set the baud rate to 115200 to view the serial stream the position from the AMT22.
+ * 
+ * Arduino Pin Connections
+ * SPI Chip Select Enc 0:   Pin  2
+ * SPI Chip Select Enc 1:   Pin  3
+ * SPI MOSI                 Pin 11
+ * SPI MISO                 Pin 12
+ * SPI SCLK:                Pin 13
+ * 
+ * 
+ * AMT22 Pin Connections
+ * Vdd (5V):                Pin  1
+ * SPI SCLK:                Pin  2
+ * SPI MOSI:                Pin  3
+ * GND:                     Pin  4
+ * SPI MISO:                Pin  5
+ * SPI Chip Select:         Pin  6
+ * 
+ * 
+ * This is free and unencumbered software released into the public domain.
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ * 
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
 /* Include the SPI library for the arduino boards */
 #include <SPI.h>
 
@@ -27,7 +83,8 @@
 #define SPI_MISO        12
 #define SPI_SCLK        13
 
-void setup() {
+void setup() 
+{
   //Set the modes for the SPI IO
   pinMode(SPI_SCLK, OUTPUT);
   pinMode(SPI_MOSI, OUTPUT);
@@ -56,128 +113,8 @@ void setup() {
   SPI.begin();
 }
 
-/*
- * This function sets the state of the SPI line. It isn't necessary but makes the code more readable than having digitalWrite everywhere 
- * This function takes the pin number of the desired device as an input
- * THIS FUNCTION IS UNNECCESSARY. WE HAVE THE AMT222B WHICH IS 14-bit
- */
-void setCSLine (uint8_t encoder, uint8_t csLine)
+void loop() 
 {
-  digitalWrite(encoder, csLine);
-}
-
-/*
- * This function does the SPI transfer. sendByte is the byte to transmit. 
- * Use releaseLine to let the spiWriteRead function know if it should release
- * the chip select line after transfer.  
- * This function takes the pin number of the desired device as an input
- * The received data is returned.
- */
-uint8_t spiWriteRead(uint8_t sendByte, uint8_t encoder, uint8_t releaseLine)
-{
-  //holder for the received over SPI
-  uint8_t data;
-
-  //set cs low, cs may already be low but there's no issue calling it again except for extra time
-  setCSLine(encoder ,LOW);
-
-  //There is a minimum time requirement after CS goes low before data can be clocked out of the encoder.
-  //We will implement that time delay here, however the arduino is not the fastest device so the delay
-  //is likely inherantly there already
-  delayMicroseconds(3);
-
-  //send the command  
-  data = SPI.transfer(sendByte);
-  delayMicroseconds(3); //There is also a minimum time after clocking that CS should remain asserted before we release it
-  setCSLine(encoder, releaseLine); //if releaseLine is high set it high else it stays low
-  
-  return data;
-}
-
-/*
- * This function gets the absolute position from the AMT22 encoder using the SPI bus. The AMT22 position includes 2 checkbits to use
- * for position verification. Both 12-bit and 14-bit encoders transfer position via two bytes, giving 16-bits regardless of resolution.
- * For 12-bit encoders the position is left-shifted two bits, leaving the right two bits as zeros. This gives the impression that the encoder
- * is actually sending 14-bits, when it is actually sending 12-bit values, where every number is multiplied by 4. 
- * This function takes the pin number of the desired device as an input
- * This funciton expects res12 or res14 to properly format position responses.
- * Error values are returned as 0xFFFF
- */
-uint16_t getPositionSPI(uint8_t encoder, uint8_t resolution)
-{
-  uint16_t currentPosition;       //16-bit response from encoder
-  bool binaryArray[16];           //after receiving the position we will populate this array and use it for calculating the checksum
-
-  //get first byte which is the high byte, shift it 8 bits. don't release line for the first byte
-  currentPosition = spiWriteRead(AMT22_NOP, encoder, false) << 8;   
-
-  //this is the time required between bytes as specified in the datasheet.
-  //We will implement that time delay here, however the arduino is not the fastest device so the delay
-  //is likely inherantly there already
-  delayMicroseconds(3);
-
-  //OR the low byte with the currentPosition variable. release line after second byte
-  currentPosition |= spiWriteRead(AMT22_NOP, encoder, true);
-  
-  //run through the 16 bits of position and put each bit into a slot in the array so we can do the checksum calculation
-  for(int i = 0; i < 16; i++) binaryArray[i] = (0x01) & (currentPosition >> (i));
-
-  //using the equation on the datasheet we can calculate the checksums and then make sure they match what the encoder sent
-  if ((binaryArray[15] == !(binaryArray[13] ^ binaryArray[11] ^ binaryArray[9] ^ binaryArray[7] ^ binaryArray[5] ^ binaryArray[3] ^ binaryArray[1]))
-          && (binaryArray[14] == !(binaryArray[12] ^ binaryArray[10] ^ binaryArray[8] ^ binaryArray[6] ^ binaryArray[4] ^ binaryArray[2] ^ binaryArray[0])))
-    {
-      //we got back a good position, so just mask away the checkbits
-      currentPosition &= 0x3FFF;
-    }
-  else
-  {
-    currentPosition = 0xFFFF; //bad position
-  }
-
-  //If the resolution is 12-bits, and wasn't 0xFFFF, then shift position, otherwise do nothing
-  if ((resolution == RES12) && (currentPosition != 0xFFFF)) currentPosition = currentPosition >> 2;
-
-  return currentPosition;
-}
-
-/*
- * The AMT22 bus allows for extended commands. The first byte is 0x00 like a normal position transfer, but the 
- * second byte is the command.  
- * This function takes the pin number of the desired device as an input
- */
-void setZeroSPI(uint8_t encoder)
-{
-  spiWriteRead(AMT22_NOP, encoder, false);
-
-  //this is the time required between bytes as specified in the datasheet.
-  //We will implement that time delay here, however the arduino is not the fastest device so the delay
-  //is likely inherantly there already
-  delayMicroseconds(3); 
-  
-  spiWriteRead(AMT22_ZERO, encoder, true);
-  delay(250); //250 second delay to allow the encoder to reset
-}
-
-/*
- * The AMT22 bus allows for extended commands. The first byte is 0x00 like a normal position transfer, but the 
- * second byte is the command.  
- * This function takes the pin number of the desired device as an input
- */
-void resetAMT22(uint8_t encoder)
-{
-  spiWriteRead(AMT22_NOP, encoder, false);
-
-  //this is the time required between bytes as specified in the datasheet.
-  //We will implement that time delay here, however the arduino is not the fastest device so the delay
-  //is likely inherantly there already
-  delayMicroseconds(3); 
-  
-  spiWriteRead(AMT22_RESET, encoder, true);
-  
-  delay(250); //250 second delay to allow the encoder to start back up
-}
-
-void loop() {
   //create a 16 bit variable to hold the encoders position
   uint16_t encoderPosition;
   //let's also create a variable where we can count how many times we've tried to obtain the position in case there are errors
@@ -253,4 +190,124 @@ void loop() {
     //delay() is in milliseconds
     delay(500);
   }
+}
+
+/*
+ * This function gets the absolute position from the AMT22 encoder using the SPI bus. The AMT22 position includes 2 checkbits to use
+ * for position verification. Both 12-bit and 14-bit encoders transfer position via two bytes, giving 16-bits regardless of resolution.
+ * For 12-bit encoders the position is left-shifted two bits, leaving the right two bits as zeros. This gives the impression that the encoder
+ * is actually sending 14-bits, when it is actually sending 12-bit values, where every number is multiplied by 4. 
+ * This function takes the pin number of the desired device as an input
+ * This funciton expects res12 or res14 to properly format position responses.
+ * Error values are returned as 0xFFFF
+ */
+uint16_t getPositionSPI(uint8_t encoder, uint8_t resolution)
+{
+  uint16_t currentPosition;       //16-bit response from encoder
+  bool binaryArray[16];           //after receiving the position we will populate this array and use it for calculating the checksum
+
+  //get first byte which is the high byte, shift it 8 bits. don't release line for the first byte
+  currentPosition = spiWriteRead(AMT22_NOP, encoder, false) << 8;   
+
+  //this is the time required between bytes as specified in the datasheet.
+  //We will implement that time delay here, however the arduino is not the fastest device so the delay
+  //is likely inherantly there already
+  delayMicroseconds(3);
+
+  //OR the low byte with the currentPosition variable. release line after second byte
+  currentPosition |= spiWriteRead(AMT22_NOP, encoder, true);        
+
+  //run through the 16 bits of position and put each bit into a slot in the array so we can do the checksum calculation
+  for(int i = 0; i < 16; i++) binaryArray[i] = (0x01) & (currentPosition >> (i));
+
+  //using the equation on the datasheet we can calculate the checksums and then make sure they match what the encoder sent
+  if ((binaryArray[15] == !(binaryArray[13] ^ binaryArray[11] ^ binaryArray[9] ^ binaryArray[7] ^ binaryArray[5] ^ binaryArray[3] ^ binaryArray[1]))
+          && (binaryArray[14] == !(binaryArray[12] ^ binaryArray[10] ^ binaryArray[8] ^ binaryArray[6] ^ binaryArray[4] ^ binaryArray[2] ^ binaryArray[0])))
+    {
+      //we got back a good position, so just mask away the checkbits
+      currentPosition &= 0x3FFF;
+    }
+  else
+  {
+    currentPosition = 0xFFFF; //bad position
+  }
+
+  //If the resolution is 12-bits, and wasn't 0xFFFF, then shift position, otherwise do nothing
+  if ((resolution == RES12) && (currentPosition != 0xFFFF)) currentPosition = currentPosition >> 2;
+
+  return currentPosition;
+}
+
+/*
+ * This function does the SPI transfer. sendByte is the byte to transmit. 
+ * Use releaseLine to let the spiWriteRead function know if it should release
+ * the chip select line after transfer.  
+ * This function takes the pin number of the desired device as an input
+ * The received data is returned.
+ */
+uint8_t spiWriteRead(uint8_t sendByte, uint8_t encoder, uint8_t releaseLine)
+{
+  //holder for the received over SPI
+  uint8_t data;
+
+  //set cs low, cs may already be low but there's no issue calling it again except for extra time
+  setCSLine(encoder ,LOW);
+
+  //There is a minimum time requirement after CS goes low before data can be clocked out of the encoder.
+  //We will implement that time delay here, however the arduino is not the fastest device so the delay
+  //is likely inherantly there already
+  delayMicroseconds(3);
+
+  //send the command  
+  data = SPI.transfer(sendByte);
+  delayMicroseconds(3); //There is also a minimum time after clocking that CS should remain asserted before we release it
+  setCSLine(encoder, releaseLine); //if releaseLine is high set it high else it stays low
+  
+  return data;
+}
+
+/*
+ * This function sets the state of the SPI line. It isn't necessary but makes the code more readable than having digitalWrite everywhere 
+ * This function takes the pin number of the desired device as an input
+ */
+void setCSLine (uint8_t encoder, uint8_t csLine)
+{
+  digitalWrite(encoder, csLine);
+}
+
+/*
+ * The AMT22 bus allows for extended commands. The first byte is 0x00 like a normal position transfer, but the 
+ * second byte is the command.  
+ * This function takes the pin number of the desired device as an input
+ */
+void setZeroSPI(uint8_t encoder)
+{
+  spiWriteRead(AMT22_NOP, encoder, false);
+
+  //this is the time required between bytes as specified in the datasheet.
+  //We will implement that time delay here, however the arduino is not the fastest device so the delay
+  //is likely inherantly there already
+  delayMicroseconds(3); 
+  
+  spiWriteRead(AMT22_ZERO, encoder, true);
+  delay(250); //250 second delay to allow the encoder to reset
+}
+
+/*
+ * The AMT22 bus allows for extended commands. The first byte is 0x00 like a normal position transfer, but the 
+ * second byte is the command.  
+ * This function takes the pin number of the desired device as an input
+ */
+void resetAMT22(uint8_t encoder)
+{
+  spiWriteRead(AMT22_NOP, encoder, false);
+
+  //this is the time required between bytes as specified in the datasheet.
+  //We will implement that time delay here, however the arduino is not the fastest device so the delay
+  //is likely inherantly there already
+  delayMicroseconds(3); 
+  
+  spiWriteRead(AMT22_RESET, encoder, true);
+  
+  delay(250); //250 second delay to allow the encoder to start back up
 }
