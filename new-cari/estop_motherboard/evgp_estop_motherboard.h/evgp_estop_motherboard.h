a4c63fcfbada5b93b8928f79a08dbb2ddc1c4416
Josiah-Koh <56281027+Josiah-Koh@users.noreply.github.com>
2020-01-20 19:20:02 -0500

    added the .h file to estop_motherboard

---
+++ new-cari/estop_motherboard/evgp_estop_motherboard.h/evgp_estop_motherboard.h
@@ -0,0 +1,19 @@
+#define SAFE_RB 3
+#define START_RB 2
+#define STEERING_IN 0
+#define DRIVE_IN 1
+
+#define STEERING_EN 10
+#define DRIVE_EN 9
+#define BRAKE_EN 6
+
+#define SENSOR_1 8
+#define SENSOR_2 12
+
+#define STACK_1 A0
+#define STACK_2 A1
+#define STACK_3 A2
+
+#define INT 7
+#define LED TXLED
+
