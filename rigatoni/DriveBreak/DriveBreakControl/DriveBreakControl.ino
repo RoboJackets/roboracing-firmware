225e38c1d5ca08952b76c4e2d908007710997ada
Parker Qi <parker.qi@hotmail.com>
2020-01-27 21:07:25 -0500

    Getspeed method update

--- rigatoni/DriveBreak/DriveBreakControl/DriveBreakControl.ino
+++ rigatoni/DriveBreak/DriveBreakControl/DriveBreakControl.ino
@@ -1,6 +1,5 @@
 #include "DriveBrake.h"
-
-
+volatile long encoder0Pos=0;
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -11,13 +10,30 @@ double accelerate(double a){
   
 }
 
-double brake(){
+double brake(double targetSpeed){
   
 }
 
 double getSpeed(){  
-  int counts = 
+  digitalWrite(encoder0PinA, HIGH);
+  digitalWrite(encoder0PinB, HIGH);
+  //encoder counts 64 every revolution
+  attachInterrupt(digitalPinToInterrupt(ENCODER_A), doEncoder, RISING); // interrupts encoderpin in 19
+  int newposition = encoder0Pos;
+  int newtime = (0.001*millis());
+  int counts = (newposition-oldposition)/(newtime-oldtime);
+  
+
 }
+
+void doencoder(){
+  if (digitalRead(encoder0PinA) == digitalRead(encoder0PinB)) {
+    encoder0Pos++;
+  } else {
+    encoder0Pos--;
+  }
+}
+
 
 void setMotorPWM(double voltage){
  byte PWM = ((0.556 - sqrt(0.556*0.556 - 0.00512*(62.1-voltage)))/0.00256); //see "rigatoni PWM to motor power" in drive for equation
