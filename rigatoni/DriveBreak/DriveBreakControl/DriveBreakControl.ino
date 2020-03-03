72f459f20171bc2c2a67164612a97f94221bd2b5
Nathan Jenkins <nrjenkins9@gmail.com>
2020-03-02 19:37:11 -0500

    Added RJNet to repository

--- rigatoni/DriveBreak/DriveBreakControl/DriveBreakControl.ino
+++ rigatoni/DriveBreak/DriveBreakControl/DriveBreakControl.ino
@@ -1,4 +1,9 @@
 #include "DriveBrake.h"
+#include "RJNet.h"
+const char RJNet::startMarker = '$';
+const char RJNet::endMarker = ';';
+
+
 volatile long encoder0Pos=0;
 float speed = 0;	// 0 - 255
 
@@ -112,4 +117,77 @@ void executeStateMachine(){
 			// create state transition conditions	
 		}
 	}
+}
+
+void RJNet() {}
+
+/*
+ *  Receives data from client you are connected to in a safe manner.
+ *  @Note: not named read() to limit confusion with Arduino Stream library interface
+ *
+ *  client: Serial, ethernet client or server, other communication method
+ */
+String RJNet::readData(Stream &client) {
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    char rc;
+    const uint8_t numChars = 128; //buffer size
+    char receivedChars[numChars]; //input buffer
+    boolean newData = false;
+
+    while (client.available() > 0 && newData == false) {
+        rc = client.read();
+
+        if (recvInProgress == true) {
+            if (rc != endMarker) {
+                receivedChars[ndx] = rc;
+                ndx++;
+                if (ndx >= numChars) {
+                    ndx = numChars - 1;
+                }
+            }
+            else {
+                receivedChars[ndx] = '\0'; // terminate the string
+                recvInProgress = false;
+                ndx = 0;
+                newData = true;
+            }
+        }
+
+        else if (rc == startMarker) {
+            recvInProgress = true;
+        }
+    }
+
+    if (newData) {
+      return receivedChars;
+    } else {
+      return "";
+    }
+}
+
+/*
+ * Sends info to client (Serial, Ethernet, etc output)
+ * @Note: not named print() to limit confusion with Arduino Stream library interface
+ * client: Serial, ethernet client or server, other communication method
+ * msg: Message to send
+ */
+void RJNet::sendData(Stream &client, String msg) {
+    client.print(addMarkersToMessage(msg));
+}
+
+/*
+ * Formats String message to have start and end markers.
+ * msg: Message to send
+ */
+String RJNet::addMarkersToMessage(String msg) {
+    //add markers
+    if (msg.charAt(0) != startMarker) {
+        msg = startMarker + msg;
+    }
+    if (msg.charAt(msg.length() - 1) != endMarker) {
+        msg = msg + endMarker;
+    }
+
+    return msg;
 }
