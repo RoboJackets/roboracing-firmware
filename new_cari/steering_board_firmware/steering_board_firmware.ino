9cfe74217ba25c26f1bace16c01a9700090b979c
gmadathany <41121956+gmadathany@users.noreply.github.com>
2021-02-15 20:58:28 -0500

    interrupt needs to be fixed

--- new_cari/steering_board_firmware/steering_board_firmware.ino
+++ new_cari/steering_board_firmware/steering_board_firmware.ino
@@ -63,27 +63,26 @@ void setup() {
   // enable timer compare interrupt
   TIMSK0 |= (1 << OCIE0A);
 
-//  //set timer1 interrupt at 1Hz
-//  TCCR1A = 0;// set entire TCCR1A register to 0
-//  TCCR1B = 0;// same for TCCR1B
-//  TCNT1  = 0;//initialize counter value to 0
-//  // set compare match register for 1hz increments
-//  OCR1A = 15624;// = (16*10^6) / (1*1024) - 1 (must be <65536)
-//  // turn on CTC mode
-//  TCCR1B |= (1 << WGM12);
-//  // Set CS12 and CS10 bits for 1024 prescaler
-//  TCCR1B |= (1 << CS12) | (1 << CS10);  
-//  // enable timer compare interrupt
-//  TIMSK1 |= (1 << OCIE1A);
-//  
-//  sei();//allow interrupts
+  //set timer1 interrupt at 1Hz
+  TCCR1A = 0;// set entire TCCR1A register to 0
+  TCCR1B = 0;// same for TCCR1B
+  TCNT1  = 0;//initialize counter value to 0
+  // set compare match register for 1hz increments
+  OCR1A = 15624;// = (16*10^6) / (1*1024) - 1 (must be <65536)
+  // turn on CTC mode
+  TCCR1B |= (1 << WGM12);
+  // Set CS12 and CS10 bits for 1024 prescaler
+  TCCR1B |= (1 << CS12) | (1 << CS10);  
+  // enable timer compare interrupt
+  TIMSK1 |= (1 << OCIE1A);
+  
+  sei();//allow interrupts
 }
  
 void loop() {
   Serial.print(toggle1);
   readEthernet();  // check for new angle from ethernet
   assignDirection(); 
-  pulse();
 }
 
 void readEthernet(){ 
