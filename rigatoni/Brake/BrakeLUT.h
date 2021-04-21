12cb173e356580eabc390a6d50d3e01398b29f94
Iwilsonp <peteridewilson@gmail.com>
2021-04-21 15:25:28 -0400

    updated LUT to maybe better values

--- rigatoni/Brake/BrakeLUT.h
+++ rigatoni/Brake/BrakeLUT.h
@@ -7,7 +7,7 @@ const byte BrakeLUTMaxIndex = BrakeLUTLength - 1;
 const float BrakeLUT[BrakeLUTLength][2] = {
   {100, 0.0},
   {300, 50},
-  {1000, 800}
+  {630, 300}
 };
 
 const int MAX_STEPPER_POSITION_FROM_HOME = (int) BrakeLUT[BrakeLUTMaxIndex][0];
