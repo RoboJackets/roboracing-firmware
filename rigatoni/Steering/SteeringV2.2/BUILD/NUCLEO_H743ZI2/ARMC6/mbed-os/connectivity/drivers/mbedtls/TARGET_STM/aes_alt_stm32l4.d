0da1c37482d27386972ea6a9c70d18c1020564d8
Iwilsonp <peteridewilson@gmail.com>
2022-04-27 23:56:55 -0400

    with removed build files

--- rigatoni/Steering/SteeringV2.2/BUILD/NUCLEO_H743ZI2/ARMC6/mbed-os/connectivity/drivers/mbedtls/TARGET_STM/aes_alt_stm32l4.d
+++
@@ -1,3 +0,0 @@
-BUILD\NUCLEO_H743ZI2\ARMC6\mbed-os\connectivity\drivers\mbedtls\TARGET_STM\aes_alt_stm32l4.o: \
-  mbed-os\connectivity\drivers\mbedtls\TARGET_STM\aes_alt_stm32l4.c \
-  BUILD\NUCLEO_H743ZI2\ARMC6\mbed_config.h
--- rigatoni/Steering/SteeringV2.2/BUILD/NUCLEO_H743ZI2/ARMC6/mbed-os/platform/source/newlib_nano_malloc_workaround.d
+++
@@ -1,3 +0,0 @@
-BUILD\NUCLEO_H743ZI2\ARMC6\mbed-os\platform\source\newlib_nano_malloc_workaround.o: \
-  mbed-os\platform\source\newlib_nano_malloc_workaround.c \
-  BUILD\NUCLEO_H743ZI2\ARMC6\mbed_config.h
