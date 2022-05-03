361610773d0d057ca2c86623bbb55f364f02689d
opatil-hub <opatil31@gatech.edu>
2022-05-02 20:47:55 -0400

    Added udp files

---
+++ rigatoni/Steering/rjnet_mbed_udp.cpp
@@ -0,0 +1,123 @@
+#include <mbed.h>
+#include "EthernetInterface.h"
+#include "rjnet_mbed_udp.h"
+
+//Initialize static members
+EthernetInterface RJNetMbed::ethernet_port;
+UDPSocket RJNetMbed::network_socket;
+
+//Time between sends
+const Kernel::Clock::duration_u32 RJNetMbed::TIME_BETWEEN_SENDS = 100ms;
+
+//Private Constants
+const SocketAddress RJNetMbed::rjnet_netmask("255.255.255.0");
+const SocketAddress RJNetMbed::rjnet_default_gateway("0.0.0.0");
+
+//Functions
+
+RJNetMbed::RJNetMbed(const SocketAddress & our_ip_addr, void (*process_one_message)(const SocketAddress &, const char[], unsigned int))
+    : our_ip_address(our_ip_addr), process_single_message(process_one_message){
+}
+
+void RJNetMbed::start_network_and_listening_threads(){
+    
+    //Sets static IP and implicitly disables DHCP
+    ethernet_port.set_network(our_ip_address, rjnet_netmask, rjnet_default_gateway);
+    ethernet_port.set_blocking(false);  //Don't block so we can print while waiting to connect
+    ethernet_port.connect();
+
+    //Wait for connection to come up
+    nsapi_connection_status_t eth_connection_status = ethernet_port.get_connection_status();
+    while(!(eth_connection_status == NSAPI_STATUS_LOCAL_UP || eth_connection_status == NSAPI_STATUS_GLOBAL_UP)){
+        printf("Ethernet not connected. Our IP: %s Error code: %d\n", our_ip_address.get_ip_address(), eth_connection_status);
+        ThisThread::sleep_for(100ms);
+        eth_connection_status = ethernet_port.get_connection_status();
+    }
+    
+
+    printf("Got ethernet link connection. Our IP: %s\n", our_ip_address.get_ip_address());
+
+    nsapi_error_t opened_network_socket = network_socket.open(&ethernet_port);
+    while(opened_network_socket != NSAPI_ERROR_OK){
+        printf("Network connection open but opening socket failed with error: %d\n", opened_network_socket);
+        ThisThread::sleep_for(1s);
+        opened_network_socket = network_socket.open(&ethernet_port);
+    }
+
+    //Bind the socket to the RJNet port
+    nsapi_error_t bound_network_socket = network_socket.bind(RJNET_DEFAULT_PORT);
+    while(bound_network_socket != NSAPI_ERROR_OK){
+        printf("Socket created but binding to port failed with error: %d\n", bound_network_socket);
+        ThisThread::sleep_for(500ms);
+        bound_network_socket = network_socket.bind(RJNET_DEFAULT_PORT);
+    }
+
+    udp_receiving_thread.start(callback(listen_for_new_messages, this));
+
+}
+
+bool RJNetMbed::send_single_message(string message, SocketAddress send_to){
+    //Send string message to the IP address send_to. Blocks until message send or socket error.
+    //Does not block or throw an error if the IP address is not connected to the network.
+    send_to.set_port(RJNET_DEFAULT_PORT);  //Set right port
+
+    int num_bytes_to_send = message.length();
+    nsapi_size_or_error_t num_bytes_sent = network_socket.sendto(send_to, message.c_str(), num_bytes_to_send);
+
+    //Print any errors
+    if(num_bytes_sent < 0){
+        printf("Error when sending: %d\n", num_bytes_sent);
+    }
+    else if(num_bytes_sent < num_bytes_to_send){
+        printf("Only sent partial message\n");
+    }
+    
+    return num_bytes_to_send == num_bytes_sent;
+}
+
+
+bool RJNetMbed::are_ip_addrs_equal(const SocketAddress & address_a, const SocketAddress & address_b){
+    //Returns true if the two SocketAddress objects have the same IP ADDRESS, ignoring port
+    nsapi_addr_t a_ip = address_a.get_addr();
+    nsapi_addr_t b_ip = address_b.get_addr();
+    if(a_ip.version == NSAPI_UNSPEC || a_ip.version != b_ip.version){
+        //Not same IP versions or both are unspecified. Either case is false
+        return false;
+    }
+
+    //How many bytes in IP address?
+    uint8_t num_bytes_in_addr = a_ip.version == NSAPI_IPv4 ? 4 : 16;
+
+    for(unsigned int i = 0; i < num_bytes_in_addr; i++){
+        if(a_ip.bytes[i] != b_ip.bytes[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+//Private functions
+
+//Thread for listening for incoming messages. Calls process_single_message
+//when message recieved.
+void RJNetMbed::listen_for_new_messages(RJNetMbed * rjnet_obj){
+    //Set up buffer for received message
+    char udp_message_received[RJNetMbed::MAX_RJNET_MESSAGE_LEN_BYTES] = {0};
+
+    while (true){
+        //This blocks indefinitely until a message is received
+        SocketAddress senders_addr;
+        nsapi_size_or_error_t receive_bytes_read = network_socket.recvfrom(&senders_addr, udp_message_received, MAX_RJNET_MESSAGE_LEN_BYTES);
+
+        //Now process the message received
+        if(receive_bytes_read >= 0){
+            //Got a valid message
+            //Get the sender's address
+            rjnet_obj->process_single_message(senders_addr, udp_message_received, receive_bytes_read);
+        }
+        else{
+            //Possible network error types: https://os.mbed.com/docs/mbed-os/v6.15/mbed-os-api-doxy/group__netsocket.html#gac21eb8156cf9af198349069cdc7afeba
+            printf("UDP receive error. Error code: %d\n", receive_bytes_read);
+        }
+    }
+}
---
+++ rigatoni/Steering/rjnet_mbed_udp.h
@@ -0,0 +1,73 @@
+#pragma once
+#include <mbed.h>
+#include "EthernetInterface.h"
+#include <string>
+
+//Possible network error types: https://os.mbed.com/docs/mbed-os/v6.15/mbed-os-api-doxy/group__netsocket.html#gac21eb8156cf9af198349069cdc7afeba
+//UDPSocket reference: https://os.mbed.com/docs/mbed-os/v6.15/apis/udpsocket.html
+//SocketAddress reference: https://os.mbed.com/docs/mbed-os/v6.15/apis/socketaddress.html
+//EthernetInterface: https://os.mbed.com/docs/mbed-os/v6.15/apis/ethernet.html
+//Threads and setting flags: https://os.mbed.com/docs/mbed-os/v6.15/apis/thread.html
+//ThisThread and waiting for flags: https://os.mbed.com/docs/mbed-os/v6.15/apis/thisthread.html
+
+//The IPs of all the boards in the system
+//These CANNOT be part of the class if they are passed to the constructor
+//or your code will compile but the address will be uninitialized
+const SocketAddress nucIP("192.168.20.2");
+const SocketAddress estopIP("192.168.20.3");
+const SocketAddress driveIP("192.168.20.4");
+const SocketAddress steeringIP("192.168.20.5");
+const SocketAddress manualIP("192.168.20.6");
+const SocketAddress brakeIP("192.168.20.7");
+//Send to this address to send a broadcast message
+const SocketAddress broadcastIP("192.168.20.255");
+
+class RJNetMbed {
+    //Values of static const constants are in the .cpp file because c++ syntax
+    public:
+
+        static const SocketAddress rjnet_netmask;     //Set the netmask.
+        static const SocketAddress rjnet_default_gateway;   //Can set gateway to 0.0.0.0 since not communicating beyond the LAN.
+
+        //Desired time between scheduled message sends
+        static const Kernel::Clock::duration_u32 TIME_BETWEEN_SENDS;
+        //RJNet messages must be shorter than this:
+        static const uint16_t MAX_RJNET_MESSAGE_LEN_BYTES = 1460;
+        //The default port for RJNET messages
+        static const uint16_t RJNET_DEFAULT_PORT = 8888;
+
+        //Constructor. First arg is our IP address, next arg is the function to call whenever we get a message
+        RJNetMbed(const SocketAddress &, void (*) (const SocketAddress &, const char[], unsigned int));
+
+        //Call this ONCE. Sets up interfaces, binds socket, and starts listening thread
+        //Blocks until Ethernet link connected
+        void start_network_and_listening_threads();
+
+        //Sends a UDP message to the target IP address. Make sure the message is not too long
+        bool send_single_message(string, SocketAddress);
+
+        //Checks IP addresses for equality ignoring port numbers
+        static bool are_ip_addrs_equal(const SocketAddress &, const SocketAddress &);
+
+    private:
+        //Internal constants
+        //Our IP address
+        const SocketAddress our_ip_address;
+
+        //Ethernet port and socket
+        static EthernetInterface ethernet_port;
+        static UDPSocket network_socket;
+
+        //This is a pointer to the function that gets called whenever we get a new messgae
+        //The arguments are (Address of sender, the message as a character array, length of the message)
+        void (*process_single_message)(const SocketAddress &, const char[], unsigned int);
+
+        //Thread to listen for new messages
+        Thread udp_receiving_thread;
+
+        //Listens for new messages. Calls process_single_message when it gets a message.
+        //Has to be implemented as static with the argument as this object pointer because c++ is wierd
+        static void listen_for_new_messages(RJNetMbed *);
+
+
+};
