8f5f7a9e7e78aa6996a05e13d50c2960a306ec49
Iwilsonp <peteridewilson@gmail.com>
2022-05-17 18:15:31 -0400

    with axis1 configuration

--- rigatoni/Brake_STM32/BrakeLUT.h
+++ rigatoni/Brake_STM32/BrakeLUT.h
@@ -4,11 +4,12 @@ This file contains a lookup table with {brake turns from home, braking force} pa
 Needs to be calibrated; currently I made this up.
 Note that this assumes home is 1 turn away from the limit switch.
 */
-const unsigned int BrakeLUTLength = 3;
+const unsigned int BrakeLUTLength = 4;
 const unsigned int BrakeLUTMaxIndex = BrakeLUTLength - 1;
 const float BrakeLUT[BrakeLUTLength][2] = {
   {0.0, -0.000001},  //just slightly negative so function is monotonic
   {6.0, 0.0},
+  {15, 10},
   {20, 300}
 };
 
