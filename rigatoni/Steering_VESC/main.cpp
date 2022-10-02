c422617863b6db5c00a84e6352d9eca88ed565ff
Eric Dong <belericant@gmail.com>
2022-10-02 19:06:20 -0400

    positional pid

--- rigatoni/Steering_VESC/main.cpp
+++ rigatoni/Steering_VESC/main.cpp
@@ -13,25 +13,42 @@ You cannot use D0, D1 for the serial port. Printf messes with those.
 BufferedSerial vesc_serial(D14, D15, 115200);  //tx, rx, baud
 VescUart the_vesc;
 
+constexpr unsigned int TICKS_PER_REV = 42 * 48;
+constexpr float DEGREE_RATIO = 360.0 / TICKS_PER_REV;
+constexpr unsigned int kP = 75;
 
-// Blinking rate in milliseconds
-#define BLINKING_RATE     500ms
+// Refresh rate in milliseconds
+#define REFRESH_RATE     10ms
 
+
+int sgn(float n) {
+    return (n > 0) - (n < 0);
+}
 
 int main()
 {
-    // Initialise the digital pin LED1 as an output
-    DigitalOut led(LED1);
-
     //Set the Vesc's serial port
-    the_vesc.setDebugEnable(true);
+    the_vesc.setDebugEnable(false);
     the_vesc.setSerialPort(&vesc_serial);
+    the_vesc.getVescValues();
+
+    const long TACH_OFFSET = the_vesc.data.tachometer;
+    
+    float target = 180;
+    float pos = ((the_vesc.data.tachometer - TACH_OFFSET) % TICKS_PER_REV + TICKS_PER_REV) % TICKS_PER_REV * DEGREE_RATIO;
 
     while (true) {
-        led = !led;
-        the_vesc.setRPM(1500);
         the_vesc.getVescValues();
-        the_vesc.printVescValues();
-        ThisThread::sleep_for(BLINKING_RATE);
-    }
+        pos = ((the_vesc.data.tachometer - TACH_OFFSET) % TICKS_PER_REV + TICKS_PER_REV) % TICKS_PER_REV * DEGREE_RATIO;
+        float command = kP * (target - pos);
+        if (fabs(command) > 30000) {
+            command = 30000 * sgn(command);
+        }
+        if (fabs(command) < 1500 && fabs(target - pos) > 1) {
+            command = 1500 * sgn(command);
+        }
+        the_vesc.setRPM(command);
+        printf("Position: %f\n", pos);
+        ThisThread::sleep_for(REFRESH_RATE);
+    }   
 }
