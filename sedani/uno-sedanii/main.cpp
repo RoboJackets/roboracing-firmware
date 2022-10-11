bed795643273a107748554f2e64a35adc505b49d
Jackie Mac Hale <machalejackie@gmail.com>
2022-10-11 00:05:05 -0400

    Remove Servo library code which is being replaced with the builtin PwmOut library in Mbed

--- sedani/uno-sedanii/main.cpp
+++ sedani/uno-sedanii/main.cpp
@@ -5,9 +5,7 @@
 
 #include <cstdint>
 #include <mbed.h>
-#include <Servo.h>
 
-Servo servo(D4);
 PwmOut pwm_servo(D5);
 const uint_fast16_t SERVO_US_MIN = 1000;
 const uint_fast16_t SERVO_US_MAX = 2000;
@@ -15,20 +13,18 @@ const uint_fast16_t SERVO_US_MAX = 2000;
 int main() {
 
     // enable servo and set position to -90 degrees
-    servo.attach(SERVO_POS_MIN);
-    //pwm_servo.period_ms(20);
+    pwm_servo.period_ms(20);
 
     while(1) {
       // Sweep across whole servo range from -90 degrees to +90 degrees
-      for (int i = 0; i <= 10; i++){
-        servo.write(i);
-        wait_us(10000);
+      for (int i = 0; i <= 10; i++) {
         pwm_servo.pulsewidth_us(SERVO_US_MIN + (SERVO_US_MAX - SERVO_US_MIN)*i/10);
+        wait_us(10000);
       }
 
       // Sweep back from 90 degrees to -90 degrees
-      for (int i = 10; i >= 0; i--){
-        servo.write(i);
+      for (int i = 10; i >= 0; i--) {
+        //servo.write(i);
         pwm_servo.pulsewidth_us(SERVO_US_MIN + (SERVO_US_MAX - SERVO_US_MIN)*i/10);
         wait_us(10000);
       }
