b6e4ebaaded0369490dc3d83ea0b02fc1369666e
Peter <peteridewilson@gmail.com>
2022-10-23 18:42:38 -0400

    with servo sweeping

--- sedani/uno-sedanii/main.cpp
+++ sedani/uno-sedanii/main.cpp
@@ -6,29 +6,46 @@
 #include <cstdint>
 #include <mbed.h>
 
-PwmOut pwm_servo(D5);
+PwmOut pwm_servo(A3);
 const uint_fast16_t SERVO_US_MIN = 1000;
 const uint_fast16_t SERVO_US_MAX = 2000;
+const uint_fast16_t SERVO_PULSE_WIDTH_US = 20e3;
+
+void write_servo(float fraction_full_scale){
+    //Initial SERVO_PULSE_WIDTH_US is to deal with inverting it
+    uint_fast16_t pulsewidth = SERVO_US_MIN + (SERVO_US_MAX - SERVO_US_MIN)*fraction_full_scale;
+    pwm_servo.pulsewidth_us(SERVO_PULSE_WIDTH_US-pulsewidth);
+    printf("pwm is: %d\n", pulsewidth);
+}
 
 int main() {
 
     // enable servo and set position to -90 degrees
-    pwm_servo.period_ms(20);
+    pwm_servo.period_us(SERVO_PULSE_WIDTH_US);
+
+    const float max_frac = 0.75;
+    const float min_frac = 1-max_frac;
+    const float step = 0.05;
 
     while(1) {
-      // Sweep across whole servo range from -90 degrees to +90 degrees
-      for (int i = 0; i <= 10; i++) {
-        pwm_servo.pulsewidth_us(SERVO_US_MIN + (SERVO_US_MAX - SERVO_US_MIN)*i/10);
-        wait_us(10000);
-      }
+        // Sweep across whole servo range from -90 degrees to +90 degrees
+        for (float i = 0.45; i <= 0.55 + 1e-3; i+=step) {
+            write_servo(i);
+            ThisThread::sleep_for(2s);
+        }
 
-      // Sweep back from 90 degrees to -90 degrees
-      for (int i = 10; i >= 0; i--) {
-        //servo.write(i);
-        pwm_servo.pulsewidth_us(SERVO_US_MIN + (SERVO_US_MAX - SERVO_US_MIN)*i/10);
-        wait_us(10000);
-      }
 
-      wait_us(1000000);
+        write_servo(0.5);
+
+        ThisThread::sleep_for(5s);
+
+        // Sweep back from 90 degrees to -90 degrees
+        /*
+        for (float i = max_frac; i >= min_frac - 1e-3; i-=step) {
+            write_servo(i);
+            ThisThread::sleep_for(500ms);
+        }
+        ThisThread::sleep_for(1s);
+        */
     }
-  }
+}
