8f5efe0e1cee5c486acba903d05e0b32b7ca356c
Peter <peteridewilson@gmail.com>
2022-10-31 21:32:06 -0400

    with new mod math

--- rigatoni/Steering_VESC/main.cpp
+++ rigatoni/Steering_VESC/main.cpp
@@ -19,10 +19,13 @@ constexpr PinName RX = D15;
 
 constexpr unsigned int TICKS_PER_REV = 42 * 48;
 constexpr float DEGREE_RATIO = 360.0 / TICKS_PER_REV;
-constexpr unsigned int kP = 150;
+constexpr unsigned int kP = 10;
 constexpr unsigned int MAX_RPM = 30000;
 constexpr unsigned int MIN_RPM = 1500;
 constexpr float POT_ELECTRICAL_RANGE = 270;
+constexpr float POT_OFFSET = 180;
+
+constexpr float MAX_ANGLE = 70;
 
 constexpr Kernel::Clock::duration_u32 REFRESH_RATE = 10ms;
 constexpr int WATCHDOG_TIMEOUT = 50;
@@ -46,7 +49,9 @@ void process_single_message(const SocketAddress & senders_address, const char in
             char outgoing_message [64];
             sprintf(outgoing_message, "Got angle = %f", temp);
             rjnet_udp.send_single_message(outgoing_message, nucIP);
-            desired_angle.store(fmod(fmod(temp, 360.0) + 360.0, 360.0));
+            desired_angle.store(temp);
+        } else if (incoming_udp_message[0] == 'R') {
+            NVIC_SystemReset();
         }
     }
 }
@@ -74,13 +79,13 @@ void watchdog_kick_thread() {
 }
 
 int main() {
-    Watchdog::get_instance().start(WATCHDOG_TIMEOUT);
-    Thread watchdog_thread(osPriorityLow);
-    watchdog_thread.start(&watchdog_kick_thread);
+    // Watchdog::get_instance().start(WATCHDOG_TIMEOUT);
+    // Thread watchdog_thread(osPriorityLow);
+    // watchdog_thread.start(&watchdog_kick_thread);
 
     DigitalOut led1(LED1);
     DigitalOut led2(LED2);
-    AnalogIn pot(A0);
+    // AnalogIn pot(A0);
 
     if (ResetReason::get() == RESET_REASON_WATCHDOG) {
         led1 = false;
@@ -90,17 +95,20 @@ int main() {
         led2 = false;
     }
 
+    //rjnet_udp.set_debug(true);
+
     Thread network_start(osPriorityLow);
     network_start.start([&]() { rjnet_udp.start_network_and_listening_threads(); });
     
     float pot_init_avg = 0;
-    for (int i = 0; i < 100; ++i) {
-        pot_init_avg += pot.read();
-        ThisThread::sleep_for(2ms);
-    }
+    // for (int i = 0; i < 100; ++i) {
+        // pot_init_avg += pot.read();
+        // ThisThread::sleep_for(2ms);
+    // }
     pot_init_avg /= 100;
 
-    desired_angle.store(pot_init_avg * POT_ELECTRICAL_RANGE);
+    // desired_angle.store(pot_init_avg * POT_ELECTRICAL_RANGE - 180.0f + POT_OFFSET);
+    desired_angle.store(0.0);
 
     //Set the Vesc's serial port
     vesc.setDebugEnable(false);
@@ -108,17 +116,24 @@ int main() {
     vesc.getVescValues();
 
 
-    const long TACH_OFFSET = vesc.data.tachometer - (long)((pot_init_avg * POT_ELECTRICAL_RANGE)/360.0f * TICKS_PER_REV);
+    const long TACH_OFFSET = vesc.data.tachometer - (long)(((pot_init_avg * POT_ELECTRICAL_RANGE)/360.0f - 180.0f + POT_OFFSET) * TICKS_PER_REV);
     float pos = ((vesc.data.tachometer - TACH_OFFSET) % TICKS_PER_REV + TICKS_PER_REV) % TICKS_PER_REV * DEGREE_RATIO;
 
+    uint32_t print_counter = 0;
     while (true) {
+        desired_angle.store(abs_max_bound(desired_angle.load(), MAX_ANGLE));
         vesc.getVescValues();
         pos = ((vesc.data.tachometer - TACH_OFFSET) % TICKS_PER_REV + TICKS_PER_REV) % TICKS_PER_REV * DEGREE_RATIO;
         float command = kP * (desired_angle - pos);
         command = abs_max_bound<float>(command, MAX_RPM);
         if (abs(desired_angle - pos) > 1) command = abs_min_bound<float>(command, MIN_RPM);
         vesc.setRPM(command);
-        printf("Position: %f\n", pos);
+
+        if(print_counter == 25){
+            printf("Position: %f Command: %f Desired: %f\n", pos, command, desired_angle.load());
+            print_counter = 0;
+        }
+        print_counter++;
         ThisThread::sleep_for(REFRESH_RATE);
     }
 }
