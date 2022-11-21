ff9315aedbd72e41f02f31e4098d34a543f396fd
Eric Dong <belericant@gmail.com>
2022-11-21 00:38:52 -0500

    change to current control

--- rigatoni/Steering_VESC/main.cpp
+++ rigatoni/Steering_VESC/main.cpp
@@ -17,11 +17,10 @@ constexpr int BAUD_RATE = 115200;
 constexpr PinName TX = D14;
 constexpr PinName RX = D15;
 
-constexpr unsigned int TICKS_PER_REV = 42 * 48;
-constexpr float DEGREE_RATIO = 360.0 / TICKS_PER_REV;
-constexpr unsigned int kP = 150;
-constexpr unsigned int MAX_RPM = 30000;
-constexpr unsigned int MIN_RPM = 2000;
+constexpr float kP = 1;
+constexpr float kI = 0.01;
+constexpr float kD = 1;
+constexpr unsigned int MAX_AMPS = 20;
 constexpr float POT_ELECTRICAL_RANGE = 260;
 constexpr float POT_OFFSET = 0.68;
 
@@ -38,6 +37,16 @@ RJNetMbed rjnet_udp(steeringIP, &process_single_message);
 
 std::atomic<float> desired_angle;
 
+template<typename T>
+inline int sgn(T n) {
+    return (n > 0) - (n < 0);
+}
+
+template<typename T>
+inline T abs_max_bound(T val, T range) {
+    return sgn(val) * min(abs(val), abs(range));
+}
+
 void process_single_message(const SocketAddress & senders_address, const char incoming_udp_message[], unsigned int num_bytes_in_message) {
     //Parses a UDP message we just recieved. Places any received data in global variables.
     if(rjnet_udp.are_ip_addrs_equal(nucIP, senders_address)) {
@@ -49,26 +58,11 @@ void process_single_message(const SocketAddress & senders_address, const char in
             char outgoing_message [64];
             sprintf(outgoing_message, "Got angle = %f", temp);
             rjnet_udp.send_single_message(outgoing_message, nucIP);
-            desired_angle.store(temp);
+            desired_angle.store(abs_max_bound(temp, MAX_ANGLE));
         } else if (incoming_udp_message[0] == 'R') {
             NVIC_SystemReset();
         }
     }
-}
-
-template<typename T>
-inline int sgn(T n) {
-    return (n > 0) - (n < 0);
-}
-
-template<typename T>
-inline T abs_max_bound(T val, T range) {
-    return sgn(val) * min(abs(val), abs(range));
-}
-
-template<typename T>
-inline T abs_min_bound(T val, T range) {
-    return sgn(val) * max(abs(val), abs(range));
 }
 
 void watchdog_kick_thread() {
@@ -78,6 +72,16 @@ void watchdog_kick_thread() {
     }
 }
 
+inline float sample_pot() {
+    static AnalogIn pot(A4);
+    float pot_read = 0;
+    for (int i = 0; i < 100; ++i) {
+        pot_read += pot.read();
+    }
+    pot_read /= 100;
+    return pot_read;
+}
+
 int main() {
     Watchdog::get_instance().start(WATCHDOG_TIMEOUT);
     Thread watchdog_thread(osPriorityLow);
@@ -85,7 +89,6 @@ int main() {
 
     DigitalOut led1(LED1);
     DigitalOut led2(LED2);
-    AnalogIn pot(A4);
 
     if (ResetReason::get() == RESET_REASON_WATCHDOG) {
         led1 = false;
@@ -97,36 +100,30 @@ int main() {
 
     Thread network_start(osPriorityLow);
     network_start.start([&]() { rjnet_udp.start_network_and_listening_threads(); });
-    
-    float pot_read = 0;
-    for (int i = 0; i < 100; ++i) {
-        pot_read += pot.read();
-    }
-    pot_read /= 100;
 
-    desired_angle.store((pot_read - POT_OFFSET) * POT_ELECTRICAL_RANGE);
+    desired_angle.store((sample_pot() - POT_OFFSET) * POT_ELECTRICAL_RANGE);
 
     //Set the Vesc's serial port
     vesc.setDebugEnable(false);
     vesc.setSerialPort(&vesc_serial);
 
-    // "Zero Position" = current tachometer position - (current pot angle in degrees - pot angle at center )* scale to ticks
-    float pos = (pot_read - POT_OFFSET) * POT_ELECTRICAL_RANGE;
-
+    float pos = (sample_pot() - POT_OFFSET) * POT_ELECTRICAL_RANGE;
+    float prevPos = pos;
+    float I = 0;
     while (true) {
-        desired_angle.store(abs_max_bound(desired_angle.load(), MAX_ANGLE));
-        pot_read = 0;
-        for (int i = 0; i < 100; ++i) {
-            pot_read += pot.read();
-        }
-        pot_read /= 100;
-        pos = (pot_read - POT_OFFSET) * POT_ELECTRICAL_RANGE;
-        float command = kP * (abs_max_bound(desired_angle.load(), MAX_ANGLE) - pos);
-        command = abs_max_bound<float>(command, MAX_RPM);
-        if (abs(desired_angle - pos) > 5) command = abs_min_bound<float>(command, MIN_RPM);
-        else command = 0;
+        auto time = Kernel::Clock::now();
+        pos = (sample_pot() - POT_OFFSET) * POT_ELECTRICAL_RANGE;
+
+        // desired angle MUST be bounded AT READ TIME due to race conditions
+        float err = (abs_max_bound(desired_angle.load(), MAX_ANGLE) - pos);
+        I += err;
+        float command = kP * err + kD * (prevPos - pos) + abs_max_bound(kI * I, 45.0f);
+        command = abs_max_bound<float>(command, MAX_AMPS);
+
+        // potentiometer and motor positive are reversed
+        vesc.setCurrent(-command);
         printf("Position: %f Command: %f Desired: %f\n", pos, command, desired_angle.load());
-        vesc.setRPM(-command);
-        ThisThread::sleep_for(REFRESH_RATE);
+        prevPos = pos;
+        ThisThread::sleep_until(time + REFRESH_RATE);
     }
 }
