a38472384c5cf0814186414e046087c1322b58b0
Jackie Mac Hale <machalejackie@gmail.com>
2023-10-15 18:31:51 -0400

    Switched PWM library to one that uses interrupts which works with multithreading

--- filini/manual-control/main.cpp
+++ filini/manual-control/main.cpp
@@ -1,27 +1,27 @@
-#include "mbed.h"
-#include "Pulse.h"
+// This reads the steering and drive data from the Futaba controller
+// in two different threads
 
-PulseInOut steering(D6);
-PulseInOut drive(D7);
+#include "mbed.h"
+#include "PwmIn.h"
+
+PwmIn steering(D6);
+PwmIn drive(D7);
 Thread thread;
 
 void drive_thread()
 {
     while (true) {
-        printf("Drive Pulse: %d\n", drive.read_us(50000));
-        // ThisThread::sleep_for(3ms);
+        printf("Drive Pulse Width: %f\n", drive.pulsewidth());
     }
 }
 
 // main() runs in its own thread in the OS
 int main()
 {
-    // thread.start(drive_thread);
+    thread.start(drive_thread);
 
     while (true) {
-        printf("Steering Pulse: %d\n", steering.read_us(50000));
-        printf("Drive Pulse: %d\n", drive.read_us(50000));
-        // ThisThread::sleep_for(3ms);
+        printf("Steering Pulse Width: %f\n", steering.pulsewidth());
     }
 }
 
