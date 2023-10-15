68dbea08680934da7065d81c86cc488213d70fdc
Jackie Mac Hale <machalejackie@gmail.com>
2023-10-15 16:58:04 -0400

    Start of multithreaded approach for reading steering and drive data

--- filini/manual-control/main.cpp
+++ filini/manual-control/main.cpp
@@ -2,12 +2,26 @@
 #include "Pulse.h"
 
 PulseInOut steering(D6);
+PulseInOut drive(D7);
+Thread thread;
+
+void drive_thread()
+{
+    while (true) {
+        printf("Drive Pulse: %d\n", drive.read_us(50000));
+        // ThisThread::sleep_for(3ms);
+    }
+}
 
 // main() runs in its own thread in the OS
 int main()
 {
+    // thread.start(drive_thread);
+
     while (true) {
-        printf("Pulse: %d\n", steering.read_us(500000));
+        printf("Steering Pulse: %d\n", steering.read_us(50000));
+        printf("Drive Pulse: %d\n", drive.read_us(50000));
+        // ThisThread::sleep_for(3ms);
     }
 }
 
